5dc80a37428cd7097b76f0e0d0f18ac2ff03a0a1
Charles Moulliard <cmoulliard@redhat.com>
2020-04-09 18:07:54 +0200

    Fix: Update the code to include in a HashMap the Role: name, type, description

--- src/main/java/dev/snowdrop/extension/DynamicTable.java
+++ src/main/java/dev/snowdrop/extension/DynamicTable.java
@@ -2,8 +2,11 @@ package dev.snowdrop.extension;
 
 import org.asciidoctor.ast.Block;
 import org.asciidoctor.ast.Document;
+import org.asciidoctor.ast.Section;
 import org.asciidoctor.ast.StructuralNode;
 import org.asciidoctor.extension.Treeprocessor;
+import org.asciidoctor.jruby.ast.impl.BlockImpl;
+import org.asciidoctor.jruby.ast.impl.SectionImpl;
 import org.jruby.RubyProcess;
 
 import java.util.HashMap;
@@ -12,13 +15,17 @@ import java.util.Map;
 
 public class DynamicTable extends Treeprocessor {
 
+    private final static String KEYWORD_ROLE = "Command";
+    private final static String KEYWORD_TYPE = "Cluster target type";
+    private final static String KEYWORD_DESCRIPTION = "Description";
+
     @Override
     public Document process(Document document) {
         processBlock((StructuralNode) document);
         return document;
     }
 
-    private void processBlock(StructuralNode node) {
+    private void showContent(StructuralNode node) {
         System.out.println("==== FindBy \":paragraph\" selector");
         Map<Object, Object> selector = new HashMap<Object, Object>();
         selector.put("context", ":paragraph");
@@ -36,46 +43,81 @@ public class DynamicTable extends Treeprocessor {
             final StructuralNode subNode = findBy.get(j);
             System.out.println("Node content: " + subNode.getContent());
         }
+    }
 
-        System.out.println("==== Tree navigation");
-        List<StructuralNode> sections = node.getBlocks();
-        for (int i = 0; i < sections.size(); i++) {
-            final StructuralNode currentBlock = sections.get(i);
-            System.out.println("Node : " + currentBlock.getContext() + ", title \"" + currentBlock.getTitle() + "\"");
+    private void processBlock(StructuralNode node) {
+        HashMap<Integer, Role> roles = new HashMap<Integer, Role>();
 
-            if ("section".equals(currentBlock.getContext())) {
-                List<StructuralNode> subLevels = currentBlock.getBlocks();
-                for (int j = 0; j < subLevels.size(); j++) {
-                    final StructuralNode subLevel = subLevels.get(j);
-                    if (subLevel.getTitle() == null) {
-                        System.out.println("  Node : " + subLevel.getContext());
-                    } else {
-                        System.out.println("  Node : " + subLevel.getContext() + ", title \"" + subLevel.getTitle() + "\"");
+        // Define a selector to find the sections starting with name "Command"
+        Map<Object, Object> selector = new HashMap<Object, Object>();
+        selector.put("context", ":section");
+
+        // Search about the section within the document
+        List<StructuralNode> findBy = node.findBy(selector);
+
+        // Role
+        Role role;
+        int counter = 0;
+        for (int i = 0; i < findBy.size(); i++) {
+            final StructuralNode subNode = findBy.get(i);
+            role = new Role();
+            String sectionTitle = subNode.getTitle();
+            List<StructuralNode> blocks = subNode.getBlocks();
+
+            if (sectionTitle.startsWith(KEYWORD_ROLE)) {
+                // If the node's title is equal to the keyword, then extract the name
+                String[] roleName = subNode.getTitle().split(": ");
+                // System.out.println("Name: " + roleName[1]);
+                role.setName(roleName[1]);
+
+                // Iterate through the nodes to find either the block containing the paragraph
+                // where the paragraph contains the Type of the cluster
+                for (int j = 0; j < blocks.size(); j++) {
+                    final StructuralNode currentBlock = blocks.get(j);
+                    if (currentBlock instanceof BlockImpl) {
+                        // Search about the paragraph containing the "Type"
+                        String content = currentBlock.getContent().toString();
+                        if (content.startsWith(KEYWORD_TYPE)) {
+                            String[] typeName = content.split(": ");
+                            role.setType(typeName[1]);
+                        }
+                        continue;
+                    }
+
+                    if (currentBlock instanceof SectionImpl) {
+                        Section section = (SectionImpl) currentBlock;
+                        if (section.getTitle().startsWith(KEYWORD_DESCRIPTION)) {
+                            role.setDescription(section.getBlocks().get(0).getContent().toString());
+                            roles.put(++counter, role);
+                        }
+                        continue;
                     }
                 }
             }
-
-/*
-                if ("paragraph".equals(currentBlock.getContext())) {
-                    List<StructuralNode> subLevels = currentBlock.getBlocks();
-                    for (int j = 0; j < subLevels.size(); i++) {
-                        StructuralNode currentSubLevel = subLevels.get(j);
-                        currentSubLevel.getContent();
-                    }
-
-
-                    List<String> lines = ((Block) currentBlock).getLines();
-                    if (lines != null
-                            && lines.size() > 0) {
-                        blocks.set(i, convertToTable((Block) currentBlock));
-                    }
-                } else {
-                    // It's not a paragraph, so recursively descend into the child node
-                    processBlock(currentBlock);
-                }
-                            }
-                */
         }
+
+
+        for (Integer key : roles.keySet()) {
+            final Role aRole = roles.get(key);
+            System.out.println("idx: " + key + ", role name: " + aRole.getName() + " type: " + aRole.getType() + " and description: " + aRole.getDescription());
+        }
+
+        /*
+        System.out.println("=== Tree navigation");
+        List<StructuralNode> blocks = node.getBlocks();
+        for (int i = 0; i < blocks.size(); i++) {
+        final StructuralNode currentBlock = blocks.get(i);
+        if ("paragraph".equals(currentBlock.getContext())) {
+            List<String> lines = ((Block) currentBlock).getLines();
+            if (lines != null
+                    && lines.size() > 0) {
+                blocks.set(i, convertToTable((Block) currentBlock));
+            }
+        } else {
+            // It's not a paragraph, so recursively descend into the child node
+            processBlock(currentBlock);
+        }
+        */
     }
 
     public Block convertToTable(Block originalBlock) {
@@ -89,5 +131,44 @@ public class DynamicTable extends Treeprocessor {
 
         block.addRole("mytable");
         return block;
+    }
+
+    private class Role {
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getTags() {
+            return tags;
+        }
+
+        public void setTags(String tags) {
+            this.tags = tags;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        String name;
+        String type;
+        String tags;
+        String description;
     }
 }
